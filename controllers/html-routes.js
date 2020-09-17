a79cc5bf5d6319a29024f84a6060116829cd094a
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-17 23:54:31 +1000

    add login and signup pages

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,7 +1,26 @@
 module.exports = function(app) {
     app.get("/", (req, res) => {
-        res.redirect("/index.html");
+        console.log('yeyuh')
+        res.redirect("/index");
     })
+
+    app.get("/signup", (req, res) => {
+        // If the user already has an account send them to the whiteboard page
+        if (req.user) {
+        res.redirect("/index");
+        }
+        // sendFile(path.join(__dirname + '/signup.html'));
+        res.redirect("/signup.html");
+    });
+
+    app.get("/login", (req, res) => {
+        // If the user already has an account send them to the whiteboard page
+        if (req.user) {
+        res.redirect("/index");
+        }
+        // sendFile(path.join(__dirname + '/login.html'));
+        res.redirect("/login.html");
+    });
 };
 
 
