c8f65eae9d51eba8c9ac88bf59646fa02f6c5c4a
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-28 21:34:17 +1000

    comment out unnecessary code

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,27 +1,27 @@
-module.exports = function(app) {
-    app.get("/", (req, res) => {
-        console.log('yeyuh')
-        res.redirect("/index");
-    })
+// module.exports = function(app) {
+//     app.get("/", (req, res) => {
+//         console.log('yeyuh')
+//         res.redirect("/index");
+//     })
 
-    app.get("/signup", (req, res) => {
-        // If the user already has an account send them to the whiteboard page
-        if (req.user) {
-        res.redirect("/index");
-        }
-        // sendFile(path.join(__dirname + '/signup.html'));
-        res.redirect("/signup.html");
-    });
+//     app.get("/signup", (req, res) => {
+//         // If the user already has an account send them to the whiteboard page
+//         if (req.user) {
+//         res.redirect("/index");
+//         }
+//         sendFile(path.join(__dirname + '/signup.html'));
+//         // res.redirect("/signup");
+//     });
 
-    app.get("/login", (req, res) => {
-        // If the user already has an account send them to the whiteboard page
-        if (req.user) {
-        res.redirect("/index");
-        }
-        // sendFile(path.join(__dirname + '/login.html'));
-        res.redirect("/login.html");
-    });
-};
+//     app.get("/login", (req, res) => {
+//         // If the user already has an account send them to the whiteboard page
+//         if (req.user) {
+//         res.redirect("/index");
+//         }
+//         sendFile(path.join(__dirname + '/login.html'));
+//         // res.redirect("/login");
+//     });
+// };
 
 
-// module.exports = router;
+// // module.exports = router;
