f48b84d2c605c47cdd7f9dd4c446c32f586cb505
Yi Lin Lee <yilinlee19@gmail.com>
2020-10-10 17:19:33 +1100

    clean up code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,19 +5,15 @@ export default {
         return axios.post("/api/login", { email: query.email, password: query.password })
     },
     signUp: query => {
-        console.log("api query is: ", query)
         return axios.post("/api/signup", query);
     },
     loadStickies: () => {
-        console.log("API call to load all stickies")
         return axios.get("/api/sticky")
     },
     createSticky: query => {
-        console.log("API call to create new sticky")
         return axios.post("/api/sticky", query);
     },
     moveSticky: query => {
-        console.log("API call to move a sticky")
         return axios.post("/api/movesticky", query)
     }
 }; 
