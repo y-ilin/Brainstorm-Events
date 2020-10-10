10c0443df40f2af4457aeca4734dc8ae5a696961
Yi Lin Lee <yilinlee19@gmail.com>
2020-10-10 11:05:59 +1100

    update api routes for stickies

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
 export default {
-    login: function(query) {
+    login: query => {
         return axios.post("/api/login", { email: query.email, password: query.password })
     },
-    signUp: function(query) {
+    signUp: query => {
         console.log("api query is: ", query)
         return axios.post("/api/signup", query);
-  }
+    },
+    loadStickies: () => {
+        console.log("API call to load all stickies")
+        return axios.get("/api/sticky")
+    },
+    createSticky: query => {
+        console.log("API call to create new sticky")
+        return axios.post("/api/sticky", query);
+    }
 }; 
