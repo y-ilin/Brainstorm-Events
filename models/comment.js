000834330459025abdf01f6ea490dc587f6e5fb1
Yi Lin Lee <yilinlee19@gmail.com>
2020-10-14 00:06:29 +1100

    add comment model

--- models/comment.js
+++ models/comment.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  commenttext: {
+  commentId: {
     type: String,
+  },
+  commentText: {
+    type: String,
+    default: "",
     trim: true,
+  },
+  stickyId: {
+    type: String,
+  },
+  sticky: {
+    type: Schema.Types.ObjectId,
+    ref: "Sticky"
   }
 });
 
