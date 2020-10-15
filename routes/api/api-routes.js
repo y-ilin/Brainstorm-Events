b047947d597ec27d0a24168b438029d803b41255
Yi Lin Lee <yilinlee19@gmail.com>
2020-10-15 15:50:15 +1100

    deploying to heroku

--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -1,20 +1,20 @@
-// const router = require("express").Router();
-// const userController = require("../../controllers/api-routes");
-// const stickyController = require("../../controllers/sticky-api-routes");
+const router = require("express").Router();
+const userController = require("../../controllers/api-routes");
+const stickyController = require("../../controllers/sticky-api-routes");
 
-require("../../controllers/api-routes.js")(app);
-require("../../controllers/sticky-api-routes.js")(app);
+// require("../../controllers/api-routes.js")(app);
+// require("../../controllers/sticky-api-routes.js")(app);
 
-// // // Matches with "/api"
-// // router.route("/")
-// //   .get(booksController.findAll)
-// //   .post(booksController.create);
+// Matches with "/api"
+router.route("/api/login")
+  .get(booksController.findAll)
+  .post(booksController.create);
 
-// // // Matches with "/api/books/:id"
-// // router
-// //   .route("/:id")
-// //   .get(booksController.findById)
-// //   .put(booksController.update)
-// //   .delete(booksController.remove);
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
 
-// module.exports = router;
+module.exports = router;
